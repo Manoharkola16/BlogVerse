48e8a7a09b9a1cf26edaed2d49212e0641ec941d
Bhanu9912 <bhanutejagajawada038@gmail.com>
2025-11-11 19:31:50 +0530

    changed the ui

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,7 +3,8 @@ import Login from '../Components/Login/Login';
 import Register from '../Components/Register/Register';
 import Preview from '../Components/Preview/Preview';
 import { createBrowserRouter } from 'react-router-dom';
-import Home from '../Components/Home/Home';
+import Home from '../Components/Users/Home/Home';
+// import Home from '../Components/Home/Home';
 
 let Routes= createBrowserRouter([
     {
